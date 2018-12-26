d75b25299f9699645b3ce05b1c5f9a39bd834236
Anshul <anshul.jain@appdirect.com>
2018-12-26 22:58:04 +0530

    Using hashset concept.

---
+++ src/com/syssolu/dsa/geeksforgeeks/array/duplicate/UsingHashSet.java
@@ -0,0 +1,26 @@
+package com.syssolu.dsa.geeksforgeeks.array.duplicate;
+
+import java.util.HashSet;
+
+/**
+ * Java program to rotate an array by d elements
+ * TC : O(n)
+ * SC : NA
+ */
+
+/*
+ * We use the concept of Hash set which only allows the unique elements
+ * */
+
+public class UsingHashSet {
+	public static void main(String args[]) {
+		UsingHashSet usingTempArray = new UsingHashSet();
+		String[] strArray = {"abc", "def", "mno", "xyz", "pqr", "xyz", "def"};
+		HashSet<String> hashSet = new HashSet<String>();
+		for (String s : strArray) {
+			if (!hashSet.add(s)) {
+				System.out.println("Duplicate element:-> " + s);
+			}
+		}
+	}
+}
