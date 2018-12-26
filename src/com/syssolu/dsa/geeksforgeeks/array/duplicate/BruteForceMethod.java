ac416dd72fddcf3918b6f78884b8ecf9f570635d
Anshul <anshul.jain@appdirect.com>
2018-12-26 22:51:11 +0530

    Find Duplicate Elements: Brute Force Method

---
+++ src/com/syssolu/dsa/geeksforgeeks/array/duplicate/BruteForceMethod.java
@@ -0,0 +1,27 @@
+package com.syssolu.dsa.geeksforgeeks.array.duplicate;
+
+/**
+ * Java program to rotate an array by d elements
+ * TC : O(n^2)
+ * SC : NA
+ */
+
+/*
+ * We compare each element of an array with other elements. If any two elements are found equal, we declare them as duplicates.
+ * Performance is not good if an array contains lots of elements
+ * */
+
+public class BruteForceMethod {
+	public static void main(String args[]) {
+		BruteForceMethod usingTempArray = new BruteForceMethod();
+		String[] strArray = {"abc", "def", "mno", "xyz", "pqr", "xyz", "def"};
+		for (int i = 0; i < strArray.length; i++) {
+			for (int j = i + 1; j < strArray.length; j++) {
+				if (strArray[i].equalsIgnoreCase(strArray[j]) && i != j) {
+					System.out.println("Duplicate element:-> " + strArray[i]);
+				}
+			}
+		}
+	}
+
+}
