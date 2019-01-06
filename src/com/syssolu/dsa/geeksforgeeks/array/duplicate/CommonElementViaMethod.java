d1cb8a6da537db4834313f913eb4cba5399ff559
Anshul <anshul.jain@appdirect.com>
2019-01-06 22:10:20 +0530

    Find common elements via Inbuild method.

---
+++ src/com/syssolu/dsa/geeksforgeeks/array/duplicate/CommonElementViaMethod.java
@@ -0,0 +1,27 @@
+package com.syssolu.dsa.geeksforgeeks.array.duplicate;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * Java program to rotate an array by d elements
+ * TC : NA
+ * SC : NA
+ */
+
+/*
+ * We will use the inbuild methods to find the common elements
+ * */
+
+public class CommonElementViaMethod {
+	public static void main(String[] args) {
+		String[] s1 = {"ONE", "TWO", "THREE", "FOUR", "FIVE", "FOUR"};
+		String[] s2 = {"THREE", "FOUR", "FIVE", "SIX", "SEVEN", "FOUR"};
+
+		HashSet<String> s1Set = new HashSet<String>(Arrays.asList(s1));
+		HashSet<String> s2Set = new HashSet<String>(Arrays.asList(s2));
+
+		s1Set.retainAll(s2Set);
+		System.out.println("Common elements are :-> " + s1Set);
+	}
+}
