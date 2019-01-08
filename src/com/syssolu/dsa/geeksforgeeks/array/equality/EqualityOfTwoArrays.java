c1ecf2ada6aba2d6a41c047ad71edde236fddb4e
Anshul <anshul.jain@appdirect.com>
2019-01-08 17:53:46 +0530

    Equality Of Two Arrays

---
+++ src/com/syssolu/dsa/geeksforgeeks/array/equality/EqualityOfTwoArrays.java
@@ -0,0 +1,22 @@
+package com.syssolu.dsa.geeksforgeeks.array.equality;
+
+import java.util.Arrays;
+
+/**
+ * Java program to find equality of two arrays
+ * TC : NA
+ * SC : NA
+ */
+
+/*
+ * We use the inbuild methods from util.Arrays to compare this will only work if the given array having
+ * same number of elements and in same order.
+ * */
+
+public class EqualityOfTwoArrays {
+	public static void main(String args[]) {
+		String[] strArray = {"abc", "def", "mno", "xyz", "pqr", "xyz", "def"};
+		String[] strArray1 = {"abc", "def", "mno", "xyz", "pqr", "xyz", "def"};
+		System.out.println("Given arrays are ->" + Arrays.equals(strArray, strArray1));
+	}
+}
