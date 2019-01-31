c5fa6cc4b5b96ec120ae0381e6f6d849ce01240a
Anshul <anshul.jain@appdirect.com>
2019-01-31 15:13:25 +0530

    Swap the first and last character of string.

---
+++ src/com/syssolu/dsa/geeksforgeeks/string/SwapFirstLastChar.java
@@ -0,0 +1,34 @@
+package com.syssolu.dsa.geeksforgeeks.string;
+
+/**
+ * Java program to swap first and last character in string
+ * TC : NA
+ * SC : NA
+ */
+
+/*
+ * 1. Split the string with space(Assuming single space)
+ * 2. Convert the converted strings into char array
+ * 3. Swap the elements
+ * */
+
+public class SwapFirstLastChar {
+	public static void main(String args[]) {
+		String str = "check for string";
+		swap(str);
+	}
+
+	private static void swap(String str) {
+		String[] strArray = str.split(" ");
+		String output = "";
+		for (int i = 0; i < strArray.length; i++) {
+			int last = strArray[i].length();
+			char[] charArray = strArray[i].toCharArray();
+			char temp = charArray[0];
+			charArray[0] = charArray[last - 1];
+			charArray[last - 1] = temp;
+			output += " " + new String(charArray);
+		}
+		System.out.println(output);
+	}
+}
