ca0c8a7678c17ab3eecd91263f4b6ee75cacffbb
anshul.jain <anshul.jain@appdirect.com>
2024-04-25 09:49:50 +0530

    Docs changes

--- src/com/artihcsolu/dsa/geeksforgeeks/array/duplicate/BruteForceMethod.java
+++ src/com/artihcsolu/dsa/geeksforgeeks/array/duplicate/BruteForceMethod.java
@@ -7,12 +7,13 @@ package com.artihcsolu.dsa.geeksforgeeks.array.duplicate;
  */
 
 /*
- * We compare each element of an array with other elements. If any two elements are found equal, we declare them as duplicates.
+ * We compare each element of an array with other elements. If any two elements are found equal, we declare them as
+ * duplicates.
  * Performance is not good if an array contains lots of elements
  * */
 
 public class BruteForceMethod {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		String[] strArray = {"abc", "def", "mno", "xyz", "pqr", "xyz", "def"};
 		for (int i = 0; i < strArray.length; i++) {
 			for (int j = i + 1; j < strArray.length; j++) {
