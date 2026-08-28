a2fac74af63713d790c13feb4197fab13dfcf6ef
anshul-jain <anshul.java@gmail.com>
2026-08-28 15:35:27 +0530

    Find duplicate via Sorting technique

--- src/com/artihcsolu/dsa/neet/array/FindDuplicate.java
+++ src/com/artihcsolu/dsa/neet/array/FindDuplicate.java
@@ -8,9 +8,12 @@ otherwise return false.
  */
 public class FindDuplicate {
 
-    // Option 1
-    // Brute Force Method: We can check every pair of different elements in the array
-    public boolean isDuplicate(int[] nums) {
+    /* Option 1
+     Brute Force Method: We can check every pair of different elements in the array
+     * TC : O(n^2)
+     * SC : O(1)
+     */
+    public boolean hasDuplicate(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] == nums[j]) {
@@ -21,10 +24,25 @@ public class FindDuplicate {
         return false;
     }
 
+    /* Option 2
+     If we sort the array, then any duplicate values will appear next to each other.
+Sorting groups identical elements together, so we can simply check adjacent positions to detect duplicates
+     * TC : O(nlogn)
+     * SC : O(1) Or O(n)
+     */
+    public boolean hasDuplicateViaSorting(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int nums[] = {1, 2, 3, 3};
         FindDuplicate findDuplicate = new FindDuplicate();
 
-        System.out.println(findDuplicate.isDuplicate(nums));
+        System.out.println(findDuplicate.hasDuplicateViaSorting(nums));
     }
 }
